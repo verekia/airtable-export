fbefcef5cd8536054145a86b0ea249fafb6b4445
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2020-09-11 04:02:29 +0200

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# airtable-export
+# Airtable Export
+
+## Setup
+
+Create a **airtable-export.config.json** file containing your table names and the export file destinations:
+```json
+{
+  "tables": [
+    ["Your Table Name 1", "data/your-table-name-1.json"],
+    ["Your Table Name 2", "data/your-table-name-2.json"]
+  ]
+}
+```
+
+Run:
+```
+npx airtable-export
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+require('dotenv/config')
+
+const Airtable = require('airtable')
+const fse = require('fs-extra')
+const stringify = require('json-stringify-pretty-compact')
+
+const config = require(`${process.cwd()}/airtable-export.config.json`)
+
+Airtable.configure({ apiKey: process.env.AIRTABLE_PERSONAL_KEY })
+
+const base = Airtable.base(process.env.AIRTABLE_BASE_ID)
+
+const exportTableData = (tableName, fileName) => {
+  const data = {}
+  return new Promise((resolve) => {
+    base(tableName)
+      .select()
+      .eachPage(
+        (records, fetchNextPage) => {
+          records.forEach((record) => {
+            data[record.id] = {
+              ...record._rawJson.fields,
+              id: record.id,
+              createdAt: record._rawJson.createdTime,
+            }
+          })
+          fetchNextPage()
+        },
+        (err) => {
+          if (err) {
+            console.error(err)
+            return
+          }
+          fse.outputFileSync(fileName, stringify(data))
+          resolve(true)
+        }
+      )
+  })
+}
+
+const asyncForEach = async (array, callback) => {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array)
+  }
+}
+
+const main = async () => {
+  asyncForEach(config.tables, async ([tableName, exportPath]) =>
+    exportTableData(tableName, exportPath)
+  )
+}
+
+main()
